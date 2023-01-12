e0a178af19f09741c8c7db268453ccdcb7b0ed7f
IvorStur <90836443+IvorStur@users.noreply.github.com>
2023-01-12 21:51:12 +0100

    2 files modified

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Student {
+  name: string;
+  lastName: string;
+  class: string;
+  age: number;
+  birthDate: Date;
+  department: string;
+  gender: string;
+  gradeAverage: number;
+  disable: string;
+  awards: string[];
+  lastEdit: Date;
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.students = [
+      {
+        name: 'John',
+        lastName: 'Doe',
+        class: '12th',
+        age: Math.floor(Math.random() * 100),
+        birthDate: new Date('2019-08-13'),
+        department: 'Science',
+        gender: 'Male',
+        gradeAverage: Math.random() * 100,
+        disable: 'No',
+        awards: ['First prize in science fair'],
+        lastEdit: new Date(),
+      },
+    ];
+    console.log(this.students);
   }
 
+  students: Student[];
 }
