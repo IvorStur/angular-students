8afcdd639802584da9e56f1d7739f6b2fc57ba72
IvorStur <90836443+IvorStur@users.noreply.github.com>
2023-01-15 10:29:15 +0100

    2 files modified

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Student {
+  name: string;
+  lastName: string;
+  class: string;
+  age: number;
+  birthDate: Date;
+  department: string;
+  gender: string;
+  gradeAverage: number;
+  disable: string;
+  awards: string[];
+  lastEdit: Date;
+}
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -12,9 +26,7 @@ export class CreateComponent implements OnInit {
 
   ngOnInit() {}
 
-  onSubmit(student: any) {
-    console.log('e');
-    console.log(student.name);
-    console.log(student);
+  onSubmit() {
+    
   }
 }
